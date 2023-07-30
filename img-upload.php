4bea960cdf1d1d5b398822a7c68090d618ecb802
256-daisuki <256mitdaisuki+git@gmail.com>
2023-07-31 03:00:20 +0900

    a

--- img-upload.php
+++ img-upload.php
@@ -1,23 +1,16 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $uploadDir = '/var/www/html/img/';
-    $extension = strtolower(pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION));
-    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif']; // 許可する拡張子のリスト
+    $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $fileName = uniqid() . '.' . $extension;
+    $filePath = $uploadDir . $fileName; // ここを修正
 
-    // アップロードされたファイルの拡張子が許可リストに含まれているかチェック
-    if (in_array($extension, $allowedExtensions)) {
-        $fileName = uniqid() . '.' . $extension;
-        $filePath = $uploadDir . $fileName;
-
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $filePath)) {
-            $imageURL = 'https://bbs.256server.com/img/' . $fileName;
-            header('Location: ' . $imageURL);
-            exit;
-        } else {
-            echo "ファイルのアップロードに失敗したよ。";
-        }
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $filePath)) {
+        $imageURL = 'https://bbs.256server.com' . '/img/' . $fileName;
+        header('Location: ' . $imageURL); // 変数名を修正
+        exit;
     } else {
-        echo "画像ファイル以外はアップロードしないでね。";
+        echo "ファイルのアップロードに失敗しました。";
     }
 }
 ?>
